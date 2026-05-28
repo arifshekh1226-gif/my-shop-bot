ca79262095867815fd916fbe1b38744d056e65cd
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-28 13:38:49 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,17 +1,12 @@
-module.exports = {
-    handlePayment: (bot) => {
-        bot.command('pay', (ctx) => {
-            const upiLink = "upi://pay?pa=xejaj@fam&pn=ShopOwner&am=100&cu=INR";
-            
-            // Yahan tum apne QR Code ki direct link daal sakte ho
-            const qrImageUrl = "https://i.postimg.cc/JzL77YPF/IMG-20260528-133515-137.jpg"; 
+module.exports = (bot) => {
+    bot.command('pay', (ctx) => {
+        const upiID = "xejaj@fam"; 
+        const name = "ShopOwner";
+        const amount = "100";
+        const note = "Key Payment";
 
-            ctx.replyWithPhoto(qrImageUrl, {
-                caption: "💰 QR Code scan karein ya neeche link par click karein:\n\n" + upiLink,
-                reply_markup: {
-                    inline_keyboard: [[{ text: "Pay via UPI App 💸", url: upiLink }]]
-                }
-            });
-        });
-    }
+        const upiLink = `upi://pay?pa=${upiID}&pn=${encodeURIComponent(name)}&am=${amount}&cu=INR&tn=${encodeURIComponent(note)}`;
+        
+        ctx.reply(`💰 Payment karne ke liye neeche diye gaye link par click karein:\n\n${upiLink}\n\n(Agar click nahi ho raha, toh link copy karke browser mein paste karein)`);
+    });
 };
