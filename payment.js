2161701d145dbdb18647cac253f92ec70ee4053e
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-28 13:29:14 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,15 +1,17 @@
 module.exports = (bot) => {
     bot.command('pay', (ctx) => {
-        const upiID = "xejaj@fam"; // Apni UPI ID yahan daal do
+        const upiID = "xejaj@fam"; 
         const name = "ShopOwner";
-        const amount = "100"; 
+        const amount = "100";
         const note = "Key Payment";
 
         const upiLink = `upi://pay?pa=${upiID}&pn=${encodeURIComponent(name)}&am=${amount}&cu=INR&tn=${encodeURIComponent(note)}`;
         
         ctx.reply("💰 Payment karne ke liye neeche link par click karein:", {
             reply_markup: {
-                inline_keyboard: [[{ text: "Pay Now 💸", url: upiLink }]]]
+                inline_keyboard: [
+                    [{ text: "Pay Now 💸", url: upiLink }]
+                ]
             }
         });
     });
