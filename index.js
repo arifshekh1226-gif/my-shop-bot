fec8987efaeeb12fdfb4bd12d84fc7ff7e20f087
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-28 13:20:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 const saveData = () => fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 
-// Modules load karo
+// ... baaki require lines ...
 require('./admin')(bot, data, saveData);
 require('./shop')(bot, data, saveData);
+require('./payment')(bot); // Ye nayi line add karo
 
 bot.start((ctx) => ctx.reply('Welcome! /buy ya /addkey use karein.'));
 bot.launch();
