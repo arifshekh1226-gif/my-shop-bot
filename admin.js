9a12331d922a8f4c8b53722872a60d6dd25c41ab
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-28 13:12:58 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const ADMIN_ID = 'YOUR_TELEGRAM_ID_HERE';
+
+module.exports = (bot, data, saveData) => {
+    bot.command('addkey', (ctx) => {
+        if (ctx.from.id.toString() !== ADMIN_ID) return ctx.reply("❌ Tum admin nahi ho!");
+        
+        const args = ctx.message.text.split(' ');
+        if (!args[1]) return ctx.reply("Usage: /addkey [key]");
+        
+        data.keys.fluorite.push(args[1]);
+        saveData();
+        ctx.reply(`✅ Key '${args[1]}' add ho gayi!`);
+    });
+};
