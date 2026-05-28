35bb42eb6126d63623c2d9e20882b68f3ea4c3d8
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-28 13:14:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const { Telegraf } = require('telegraf');
 const fs = require('fs');
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Database Load
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-
 const saveData = () => fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 
-bot.start((ctx) => ctx.reply('Welcome to the shop! /buy use karein.'));
+// Modules load karo
+require('./admin')(bot, data, saveData);
+require('./shop')(bot, data, saveData);
 
-// Buy Command
-bot.command('buy', (ctx) => {
-    if (data.keys.fluorite.length > 0) {
-        const key = data.keys.fluorite.shift();
-        saveData();
-        ctx.reply(`✅ Aapki Key: ${key}`);
-    } else {
-        ctx.reply("❌ Stock khatam ho gaya hai!");
-    }
-});
-
+bot.start((ctx) => ctx.reply('Welcome! /buy ya /addkey use karein.'));
 bot.launch();
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
