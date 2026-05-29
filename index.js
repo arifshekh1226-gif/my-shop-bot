e45a16021c1957b070ebc9de1714b4bdd8994957
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-29 09:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 const { Telegraf } = require('telegraf');
 const fs = require('fs');
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 const saveData = () => fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 
-// Start Command with All Buttons
 bot.start((ctx) => {
-    ctx.reply("👋 **Welcome to Premium Shop!**\n\nChoose an option below:", {
+    ctx.reply("👋 Welcome to Premium Shop!", {
         reply_markup: {
             inline_keyboard: [
                 [{ text: "🛒 Buy Keys", callback_data: 'buy_menu' }],
                 [{ text: "💳 Add Balance", callback_data: 'pay_menu' }],
-                [{ text: "📞 Contact Admin", url: "https://t.me/YOUR_USERNAME" }] // Yahan apna username likh
+                [{ text: "📞 Contact Admin", url: "https://t.me/cy992" }]
             ]
         }
     });
 });
 
-// Load modules
+// Load modules (Bot, Data, Save function)
 require('./admin')(bot, data, saveData);
 require('./shop')(bot, data, saveData);
 require('./payment')(bot, data, saveData);
 
-// Force Launch
-bot.launch({ dropPendingUpdates: true }).then(() => {
-    console.log("🚀 Bot is live!");
-});
+bot.launch().then(() => console.log("🚀 Bot is running!"));
