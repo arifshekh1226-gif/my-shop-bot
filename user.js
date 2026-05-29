a786c7f24c71f9fda705d87190cda344b5c02d94
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-29 10:51:21 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,21 +1,7 @@
 module.exports = (bot, data, saveData) => {
-    
-    // Account Button
     bot.action('account', (ctx) => {
         ctx.answerCbQuery();
-        const uid = ctx.from.id.toString();
-        const balance = data.users[uid] ? data.users[uid].balance : 0;
-        ctx.editMessageText(`👤 **Account Info**\n\n🆔 ID: \`${uid}\`\n💰 Balance: ₹${balance}\n\nBalance add karne ke liye 'Deposit Fund' use karein.`, {
-            parse_mode: 'Markdown',
-            reply_markup: { inline_keyboard: [[{ text: "🔙 Back", callback_data: 'main_menu' }]] }
-        });
-    });
-
-    // History Button (Filhal simple placeholder)
-    bot.action('history', (ctx) => {
-        ctx.answerCbQuery();
-        ctx.editMessageText("📜 **Purchase History**\n\nAbhi tak koi purchase nahi ki hai.", {
-            reply_markup: { inline_keyboard: [[{ text: "🔙 Back", callback_data: 'main_menu' }]] }
-        });
+        const bal = data.users[ctx.from.id]?.balance || 0;
+        ctx.editMessageText(`👤 Balance: ₹${bal}`, { reply_markup: { inline_keyboard: [[{text:"🔙 Back",callback_data:'main_menu'}]] } });
     });
 };
