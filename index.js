e1beb51f882dcf4d664d99006b1b9f003e516d82
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-05-30 13:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, session } = require('telegraf'); // 'session' import kiya
 const fs = require('fs');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Session add kar diya (Sabse upar)
+bot.use(session());
+
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 const saveData = () => fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 
@@ -18,15 +22,16 @@ const mainMarkup = {
 };
 
 bot.start((ctx) => ctx.reply("✨ **CY SHOP**\n\n🔒 Premium Gaming Keys\n⚡ Instant Delivery\n\n👋 Welcome!", mainMarkup));
+
 bot.action('main_menu', (ctx) => {
     ctx.answerCbQuery();
     ctx.editMessageText("✨ **CY SHOP**\n\n🔒 Premium Gaming Keys\n⚡ Instant Delivery\n\n👋 Welcome back!", mainMarkup);
 });
 
-// Importing modules
-require('./admin')(bot, data, saveData);
-require('./shop')(bot, data, saveData);
-require('./payment')(bot, data, saveData);
-require('./user')(bot, data, saveData);
+// Modules Import (Sirf bot pass kar raha hoon kyunki data/saveData session se handle hoga)
+require('./admin')(bot);
+require('./shop')(bot);
+require('./payment')(bot);
+require('./user')(bot);
 
 bot.launch({ dropPendingUpdates: true }).then(() => console.log("🚀 Bot is LIVE!"));
